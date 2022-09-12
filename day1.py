f4093e80a85f12957aeaa7dbcb51ae58d5258a10
honalele <dreamhona.lele@gmail.com>
2022-09-12 13:53:20 +0900

    day 1

---
+++ day1.py
@@ -0,0 +1,11 @@
+# Day #1 Challenge
+# Author: Naren
+# Date: 2022.9.12
+
+print("Welcome to the Band Name Generator.")
+print("What's name of the city you grew up in?") 
+city_name = input()
+print("What's your pet's name?")
+pet_name = input()
+band_name = city_name + " " + pet_name
+print("Your band name could be {}".format(band_name))
